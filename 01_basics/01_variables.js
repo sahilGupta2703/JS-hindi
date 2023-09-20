dae3cc6ef437e609064b8e86a61b43df9728d7a8
Sahil Gupta <144897949+sahilGupta2703@users.noreply.github.com>
2023-09-20 19:16:04 +0000

    Commiting variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail ="sahil@google.com"
+var accountPassword = "123467"
+accountCity="Jaipur"
+
+let accountState;
+// accountId=3
+accountEmail="fasfl@gsg.com"
+accountPassword="079797"
+accountCity="alwar"
+
+console.log(accountId);
+/*
+prefer not to use var because of issues in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(1) // code readability should be high
+ console.log(0)
+
+ let name = "Sahil" //String
+ let age = 25 //integer
+ let loggedIn= false //boolean
+
+ // number => 2^ 53
+ // bigint 
+ //String => ""
+ //boolean => true/false
+ //null => stand alone value (Empty)
+ //undefined => 
+ //symbol => unique
+
+
+
+ //object
+
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null); //object
